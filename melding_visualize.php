9ed7f3219c84eff6f0bc37d25c7d2f596204aab6
lekkerhammetje <70198780+lekkerhammetje@users.noreply.github.com>
2022-07-06 13:20:08 +0200

    banzinga

--- melding_visualize.php
+++ melding_visualize.php
@@ -8,14 +8,6 @@
     <script src="https://kit.fontawesome.com/fbea3c1d87.js" crossorigin="anonymous"></script>
       <?php
       include 'config.php';
-      $servername = $DB_HOST;
-      $username = $DB_USER;
-      $password = $DB_PASSWORD;
-      $databasename = $DB_NAAM;
-      $conn = new mysqli($servername, $username, $password, $databasename);
-      if ($conn->connect_error) {
-          die("Connection failed: " . $conn->connect_error);
-        }
 
       ?>
 </head>
@@ -25,9 +17,9 @@
         <div class="sidebar">
             <h2>[user-name]</h2>
             <ul>
-                <li><a href="helpdesk-dashboard/dashboard.html"><i class="fas fa-home"></i>Thuispagina</a></li>
-                <li><a href="melding_visualize.php"><i class="fas fa-layer-group"></i>Zie Meldingen</a></li>
-                <li><a href="melding_create.php"><i class="fas fa-plus"></i>Maak Melding</a></li>
+                <li><a href="#"><i class="fas fa-home"></i>Thuispagina</a></li>
+                <li><a href="#"><i class="fas fa-layer-group"></i>Zie Meldingen</a></li>
+                <li><a href="#"><i class="fas fa-plus"></i>Maak Melding</a></li>
             </ul>
 
         </div>
@@ -37,36 +29,36 @@
                 <div>
                   <table class = "table">
                   <tr>
-                          <td>User<br> </td> 
-                          <td>Titel<br> </td> 
-                          <td>Melding ID<br> </td>
-                          <td>Status<br> </td>
-                          <td>Prioriteit <br> </td>
+                          <td> Titel <br> </td> 
+                          <td> Melding ID<br> </td>
+                          <td> Status <br> </td>
                           <td>Datum <br> </td>
                           </tr>
                   
+                     
                   <?php
-                        $sql1 = "SELECT userID, meldingID, statusID, beschr_kort, datum, prioID FROM meldingen";
+
+                        
+                        $sql1 = "SELECT meldingID, statusID, beschr_kort, datum FROM meldingen";
                         $sql2 = "SELECT statusID, status FROM status";
-                        $sql3 = "SELECT prioID, prioriteit FROM prioriteit";
-                        $result3 = $conn->query($sql3);
                         $result2 = $conn->query($sql2);
                         $result = $conn->query($sql1);
-
+                        
                         while($row = $result->fetch_assoc()) {
-                          
+                            $mid = $row["meldingID"];
                           echo "<tr>";
-                          echo "<td>" .$row["userID"]."<br> </td>"; 
-                          echo "<td>" .$row["beschr_kort"]."<br> </td>"; 
+                          echo "<td>" .$row["beschr_kort"]."<br>  </td>"; 
                           echo "<td>" .$row["meldingID"]."<br> </td>";
                           echo "<td>" .$row["statusID"]."<br> </td>";
-                          echo "<td>" .$row["prioID"]."<br> </td>";                          
                           echo "<td>" .$row["datum"]."<br> </td>";
+                          echo "<td><a href='reactie.php?meldingid=$mid'>   <input type='button' value='test'></a> <br> </td>";
                           echo "</tr>";
-                            
-                  
-                          }  
+                          
+                          } 
+
                   ?>
+
+                  </input>
                   </table>
                 </div>
             </div>
