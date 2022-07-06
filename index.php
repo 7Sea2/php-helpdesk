2e2cfb1d9d5d53b4c60b2d22afb8bb13decdcf00
MartinBoersma <martinboersma55@gmail.com>
2022-07-06 12:38:51 +0200

    home

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="helpdesk-dashboard/style.css">
+    <script src="https://kit.fontawesome.com/fbea3c1d87.js" crossorigin="anonymous"></script>
+</head>
+<body>
+
+    <div class="wrapper">
+        <div class="sidebar">
+            <h2>[user-name]</h2>
+            <ul>
+                <li><a href="dashboard.html"><i class="fas fa-home"></i>Thuispagina</a></li>
+                <li><a href="../melding_visualize.php"><i class="fas fa-layer-group"></i>Zie Meldingen</a></li>
+                <li><a href="../melding_create.php"><i class="fas fa-plus"></i>Maak Melding</a></li>
+            </ul>
+
+        </div>
+        <div class="main_content">
+            <div class="header">Welcome!</div>
+            <div class="info">
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
