65b6a2a8e9558e2c22b0bff0aa4c1c60a9eedae9
MartinBoersma <martinboersma55@gmail.com>
2022-07-06 13:39:18 +0200

    insahllah

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
-<<<<<<< Updated upstream
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Dashboard</title>
-    <link rel="stylesheet" href="helpdesk-dashboard/style.css">
-    <script src="https://kit.fontawesome.com/fbea3c1d87.js" crossorigin="anonymous"></script>
-</head>
-=======
 <?php
 session_start();
 ?>
@@ -21,31 +11,21 @@ session_start();
     <script src="https://kit.fontawesome.com/fbea3c1d87.js" crossorigin="anonymous"></script>
 </head>
 
->>>>>>> Stashed changes
 <body>
 
     <div class="wrapper">
         <div class="sidebar">
             <h2>[user-name]</h2>
             <ul>
-<<<<<<< Updated upstream
-                <li><a href="dashboard.html"><i class="fas fa-home"></i>Thuispagina</a></li>
-                <li><a href="../melding_visualize.php"><i class="fas fa-layer-group"></i>Zie Meldingen</a></li>
-                <li><a href="../melding_create.php"><i class="fas fa-plus"></i>Maak Melding</a></li>
-            </ul>
-
-=======
                 <li><a href="index.php"><i class="fas fa-home"></i>Thuispagina</a></li>
                 <li><a href="melding_visualize.php"><i class="fas fa-layer-group"></i>Zie Meldingen</a></li>
                 <li><a href="melding_create.php"><i class="fas fa-plus"></i>Maak Melding</a></li>
             </ul>
             <li><a href="logoutConfirm.php" class="logout"><i class="fas fa-minus"></i>Log Out</a></li>
->>>>>>> Stashed changes
         </div>
         <div class="main_content">
             <div class="header">Welcome!</div>
             <div class="info">
-<<<<<<< Updated upstream
                 <div>
                     
                 </div>
@@ -54,7 +34,6 @@ session_start();
     </div>
 
 </body>
-=======
                 <?php
                 if ($_SESSION['loggedin']) {
                     echo "logged in succesfully";
@@ -63,17 +42,8 @@ session_start();
                     echo "logout failed";
                 }
                 ?>
-                <!--<div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet fugit, animi repudiandae placeat, molestiae incidunt praesentium unde earum vitae 
-                     maiores reprehenderit impedit amet consequatur quaerat quasi m
-                     inima numquam ratione sapiente possimus eaque illo eius. Beatae laboriosam at animi vel. Non
-                     , beatae eligendi. Esse veniam consequuntur eveniet soluta dolorem nisi corrupti autem minima nihil sunt optio 
-                     sed maiores ea quam suscipit omnis ex temporibus cum, accusantium laudantium cupiditate. Consequatur blanditiis ab non suscipit aliquid dolorem cumque harum
-                     , corrupti, repellendus qui unde!</div>
-            </div>
-        </div>-->
             </div>
 
 </body>
 
->>>>>>> Stashed changes
 </html>
--- melding_visualize.php
+++ melding_visualize.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Dashboard</title>
-    <link rel="stylesheet" href="helpdesk-dashboard/style.css">
+    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="table.css">
     <script src="https://kit.fontawesome.com/fbea3c1d87.js" crossorigin="anonymous"></script>
       <?php
@@ -17,10 +17,11 @@
         <div class="sidebar">
             <h2>[user-name]</h2>
             <ul>
-                <li><a href="helpdesk-dashboard/dashboard.html"><i class="fas fa-home"></i>Thuispagina</a></li>
+                <li><a href="index.php"><i class="fas fa-home"></i>Thuispagina</a></li>
                 <li><a href="melding_visualize.php"><i class="fas fa-layer-group"></i>Zie Meldingen</a></li>
                 <li><a href="melding_create.php"><i class="fas fa-plus"></i>Maak Melding</a></li>
             </ul>
+            <li><a href="logoutConfirm.php" class="logout"><i class="fas fa-minus"></i>Log Out</a></li>
 
         </div>
         <div class="main_content">
