fcfff3e942d87911f54fc25955522f36c5588690
riemer <70262649+mrlepel@users.noreply.github.com>
2022-07-07 13:27:11 +0200

    Update verify_user.php

--- scripts/verify_user.php
+++ scripts/verify_user.php
@@ -17,27 +17,16 @@
           if ($current_user == null)
           {
           	session_destroy();
-			$_SESSION["loggedin"] = false;
-          }
-	}
-
-	function need_login()
-	{
-
-		if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') $url = "https://";   
-	     else $url = "http://";   
-
-
-	     $url.= $_SERVER['HTTP_HOST'];   
-	    
-
-	     $url.= $_SERVER['REQUEST_URI'];  
-
-		$_SESSION["page"] = $url;
-
-		if (!$_SESSION["loggedin"])
-		{
-			header("location: inlog.php");
+			$_SESSION["loggedin"] = false;			die();
 		}
 	}
 
+	function need_admin($user)
+	{
+		need_login();
+		if ($user['rollID'] != '1')
+		{
+			header("location: unauthorized_admin.php");
+			die();
+		}
+	}
